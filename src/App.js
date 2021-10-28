47f5a0218058563ad534247b1fba63f1e9c7f323
surbaniec <radsler1999@gmail.com>
2021-10-28 22:03:09 +0200

    add tailwind, tailwind config, create Login page, add router, add postcss config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTER from './constants/routes';
+
+const Login = lazy(() => import('./pages/Login'));
+
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTER.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
