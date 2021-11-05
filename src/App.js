918e7f02ed10ebdcffa1fdae0253d8f3b8fb20eb
surbaniec <radsler1999@gmail.com>
2021-11-05 11:25:01 +0100

    add use-auth-listener hook, add user context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTER from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
 
 const Login = lazy(() => import('./pages/Login'));
 const Signup = lazy(() => import('./pages/Signup'));
 const NotFound = lazy(() => import('./pages/NotFound'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTER.LOGIN} component={Login} />
-          <Route path={ROUTER.SIGN_UP} component={Signup} />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTER.LOGIN} component={Login} />
+            <Route path={ROUTER.SIGN_UP} component={Signup} />
+            <Route path={ROUTER.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import Timeline from '../components/Timeline';
+
+const Dashboard = () => {
+  useEffect(() => {
+    document.title = 'Instagram';
+  }, []);
+  return (
+    <div className='bg-gray-background'>
+      <Header />
+      <div className='grid'>
+        <Timeline />
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
