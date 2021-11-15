ca67c8299eaae28843b5fc3dcdc6f363cab334ea
surbaniec <radsler1999@gmail.com>
2021-11-15 16:23:36 +0100

    Add use-photos custom hook

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
 
 const Timeline = () => {
+  const { photos } = usePhotos();
+
   return <div className='container col-span-2'>Timeline</div>;
 };
 
