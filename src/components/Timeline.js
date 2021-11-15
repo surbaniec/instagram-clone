af3c0fc4873004f947ead8d537bfe9e1d8f5f2e7
surbaniec <radsler1999@gmail.com>
2021-11-15 16:37:35 +0100

    add timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,7 +5,27 @@ import usePhotos from '../hooks/use-photos';
 const Timeline = () => {
   const { photos } = usePhotos();
 
-  return <div className='container col-span-2'>Timeline</div>;
+  return (
+    <div className='container col-span-2'>
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, index) => (
+            <Skeleton
+              key={index}
+              count={4}
+              width={640}
+              height={600}
+              className='mb-5'
+            />
+          ))}
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className='text-center text-2xl'>Follow people to see photos</p>
+      )}
+    </div>
+  );
 };
 
 export default Timeline;
